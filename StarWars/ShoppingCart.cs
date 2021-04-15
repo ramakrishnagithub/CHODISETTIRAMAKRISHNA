b3a336ebab3c34caeac8dd8d19eab871fa260f17
chodisettiramakrishna@gmail.com <chodisettiramakrishna@gmail.com>
2021-04-15 05:34:33 -0500

    added Zero,OneMovie Test case and implementation

--- StarWars/ShoppingCart.cs
+++ StarWars/ShoppingCart.cs
@@ -1,16 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace StarWars
 {
     public class ShoppingCart
     {
+        #region Fields
+        private const double moviePrice=10d; 
+        private readonly List<string> _movies;
+        #endregion Fields
+
+        public ShoppingCart()
+        {
+            _movies=new List<string>(10);
+        }
+
+        #region Methods
         public void AddMovie(string name)
         {
-            throw new NotImplementedException();
+            _movies.Add(name);
         }
-        public void ComputePrice()
+        public double ComputePrice()
         {
-            throw new NotImplementedException();
+           var finalAmount=0d;
+           finalAmount=_movies.Count()*moviePrice;
+           return finalAmount;
         }
+        #endregion Methods
     }
 }
--- StarWarsTest/UnitTest1.cs
+++ StarWarsTest/UnitTest1.cs
@@ -1,14 +1,26 @@
 using System;
 using Xunit;
+using StarWars;
 
 namespace StarWarsTest
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void ZeroMoviesInShoppingCart()
         {
-
+            var shopCart=new ShoppingCart();
+            double finalAmount=shopCart.ComputePrice();
+            Assert.Equal(0d,finalAmount);
         }
+        [Fact]
+        public void OneMovieInTheCartDoNotApplyDiscount()
+        {
+            var shopCart=new ShoppingCart();
+            shopCart.AddMovie("StarWars 1");
+            double finalAmount=shopCart.ComputePrice();
+            Assert.Equal(10d,finalAmount);
+        }
+        
     }
 }
