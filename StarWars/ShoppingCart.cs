41a84b7d8c5df592ff98d93a7ce4f37f93909a18
chodisettiramakrishna@gmail.com <chodisettiramakrishna@gmail.com>
2021-04-15 05:09:28 -0500

    Initial Commit

---
+++ StarWars/ShoppingCart.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StarWars
+{
+    public class ShoppingCart
+    {
+        public void AddMovie(string name)
+        {
+            throw new NotImplementedException();
+        }
+        public void ComputePrice()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ StarWarsTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace StarWarsTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
